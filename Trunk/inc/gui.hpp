53458fc33ef662e6fd2c15d520a0cd3408da694c
Amalie Kettering <amaliemkettering@gmail.com>
2017-11-18 01:07:03 -0500

    GUI bologna

--- Trunk/inc/gui.hpp
+++ Trunk/inc/gui.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <map>
+#include <string>
 
 class gui {
 public:
@@ -9,4 +11,8 @@ public:
     void update(float deltaTime);
 private:
     sf::RenderWindow* my_window;
+    std::map<std::string,sf::Sprite> background_sprites;
+    std::map<std::string,sf::Sprite> girls_sprites;
+    std::map<std::string,sf::Sprite> button_sprites;
+    sf::Font font;
 };
--- Trunk/src/gui.cpp
+++ Trunk/src/gui.cpp
@@ -2,15 +2,27 @@
 
 gui::gui(sf::RenderWindow* window) {
     my_window = window;
+    if (!font.loadFromFile("arial.ttf")) { /* error! */ }
+    // initialize maps
+    sf::Texture girl_1_u;
+    if (!texture.loadFromFile("girl_1_u.png")) { /* error! */ }
+    sf::Texture girl_1_h;
+    if (!texture.loadFromFile("girl_1_u.png")) { /* error! */ }
+    sf::Texture girl_1_s;
+    if (!texture.loadFromFile("girl_1_u.png")) { /* error! */ }
+    // put in map
+    sf::Sprite sprite;
+    sprite.setTexture(girl_1_h);
+    // put in map
 }
 
 gui::~gui() {
-
+    // delete arrays?
 }
 
 void gui::update(float deltaTime) {
     my_window->clear(sf::Color::Black);
-    // code here
+    my_window.draw(sprite)
 
     my_window->display();
 }
