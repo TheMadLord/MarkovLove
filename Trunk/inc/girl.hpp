161d3eb0dbb2ca186dcaff67669840f63a263089
Ryan Tollefson <rtollefson@localhost.localdomain>
2017-11-19 01:20:53 -0500

    added location to girl

--- Trunk/inc/girl.hpp
+++ Trunk/inc/girl.hpp
@@ -8,10 +8,16 @@
 #define GIRL_MOOD_HAPPY 2
 #define GIRL_MOOD_LOVE 3
 
+#define GIRL_LOC_MISSING -1
+#define GIRL_LOC_FORREST 0
+#define GIRL_LOC_BAKERY 1
+#define GIRL_LOC_LIBRARY 2
+
 class Girl{
 private:
     std::string name;
     int mood;
+    int location;
 public:
     Girl(std::string name);
 
@@ -24,4 +30,18 @@ public:
     void modifyMood(int change);
 
     std::string fillVars(std::string line);
+
+    void setLocation(int i){
+        if(i >= GIRL_LOC_MISSING and i<= GIRL_LOC_LIBRARY){
+            locaction = i;
+        }
+    }
+
+    int getLocation(int i){
+        return location;
+    }
+
+    void updateLocation(){
+        location = rand()%3;
+    }
 };
